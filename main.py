52dcbd1a74aa4d02f85c2154b7a0320f2b71deb4
Mr.Lem <cccccyh6048@gmail.com>
2023-11-26 21:55:56 +0000

    Remove useless codes

--- main.py
+++ main.py
@@ -8,16 +8,15 @@ import util
 
 crime_data_url = "./data/MPS Borough Level Crime (Historical).csv"
 rent_data_url = "./data/local-authority-rents-borough.xlsx"
-selected_crimes = ['Domestic Burglary', 'Drug Trafficking', 'Criminal Damage']
+selected_crimes = ['Burglary Business and Community', 'Historical Fraud and Forgery', 'Criminal Damage', 'Bicycle Theft']
 
 if __name__ == "__main__":
     crime_df = myDF.AllDataFrameInOne(rent_data_url, crime_data_url)
 
-    # # Simple linear
-    # crime_df.join_tables('Criminal Damage', 202001, '2020-21')
-    # util.simple_linear_regression(crime_df.df_reg_, 'year_total', '2020-21')
+    # Simple linear
+    crime_df.join_tables('Bicycle Theft', 202001, '2020-21')
+    util.simple_linear_regression(crime_df.reg_df_, 'year_total', '2020-21')
 
-    # Multi linear
-    crime_df.join_multi_row(selected_crimes, 202001, '2020-21')
-    print(crime_df.reg_df_.columns)
-    util.multi_linear_regression(crime_df.reg_df_, selected_crimes, '2020-21')
+    # # Multi linear
+    # crime_df.join_multi_row(selected_crimes, 202001, '2020-21')
+    # util.multi_linear_regression(crime_df.reg_df_, selected_crimes, '2020-21')
